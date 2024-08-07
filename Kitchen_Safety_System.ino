2333accb011327d029d84b99c0512a0cb9e10b87
CG <139866630+CGCurobot@users.noreply.github.com>
2024-08-07 16:10:17 +0530

    Update Kitchen_Safety_System.ino

--- Kitchen_Safety_System.ino
+++ Kitchen_Safety_System.ino
@@ -1,35 +1,49 @@
-//Kitchen Safety System
 
-int redLed = 12;           // Pin for the red LED
-int greenLed = 11;         // Pin for the green LED
-int buzzer = 10;           // Pin for the buzzer
-int smokeSensorPin = A5;   // Pin for the smoke sensor (analog pin A5)
-// Your threshold value
-int sensorThreshold = 400; // Threshold value for the smoke sensor
+//Change Blynk Authenticaation Details
+#define BLYNK_TEMPLATE_ID "ABPLmPQCdrMg"
+#define BLYNK_DEVICE_NAME "Gas Detection Alert"
+#define BLYNK_AUTH_TOKEN "fRiTEoa3v1QV9UDShh4gtfgbs34oM81i"
 
-void setup() {
-  pinMode(redLed, OUTPUT);       // Set red LED as output
-  pinMode(greenLed, OUTPUT);     // Set green LED as output
-  pinMode(buzzer, OUTPUT);       // Set buzzer as output
-  pinMode(smokeSensorPin, INPUT);// Set smoke sensor pin as input
-  Serial.begin(9600);            // Initialize serial communication at 9600 bps
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+ 
+char auth[] = BLYNK_AUTH_TOKEN;
+
+char ssid[] = "Wifi Name";  // Enter your wifi name
+char pass[] = "Wifi Password";  // Enter your wifi password
+int smoke = A0;
+int data = 0;
+int sensorThres = 100;
+
+
+BlynkTimer timer;
+
+void sendSensor(){
+ 
+ int data = analogRead(smoke);
+ Blynk.virtualWrite(V0, data);
+  Serial.print("Pin A0: ");
+  Serial.println(data);
+
+
+  if(data > 999)     // Change the Trashold value
+  {
+    Blynk.email("test@gmail.com", "Alert", "Gas Leakage Detected!");
+    Blynk.logEvent("gas_alert","Gas Leakage Detected");
+  }
 }
 
-void loop() {
-  int analogSensorValue = analogRead(smokeSensorPin); // Read the analog value from the smoke sensor
+void setup(){
+  pinMode(smoke, INPUT);
+   Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+  //dht.begin();
+  timer.setInterval(2500L, sendSensor);
+}
 
-  Serial.print("Pin A5: ");
-  Serial.println(analogSensorValue);
-  
-  // Check if the analog value exceeds the threshold
-  if (analogSensorValue > sensorThreshold) {
-    digitalWrite(redLed, HIGH);   // Turn on the red LED
-    digitalWrite(greenLed, LOW);  // Turn off the green LED
-    tone(buzzer, 1000, 200);      // Activate the buzzer with a frequency of 1000 Hz for 200 ms
-  } else {
-    digitalWrite(redLed, LOW);    // Turn off the red LED
-    digitalWrite(greenLed, HIGH); // Turn on the green LED
-    noTone(buzzer);               // Deactivate the buzzer
-  }
-  delay(100); // Wait for 100 ms before the next loop
+void loop(){
+  Blynk.run();
+  timer.run();
 }
